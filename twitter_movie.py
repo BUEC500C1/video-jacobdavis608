1f7a24843a6c1eeb9b159520622a19905f72ab13
Jacob Davis <jacobdavis608@gmail.com>
2020-02-23 01:54:51 -0500

    Beginning unparallelized version with downloads and encode function stubs

--- twitter_movie.py
+++ twitter_movie.py
@@ -1,14 +1,33 @@
 import tweepy
 import wget
 import os
+import threading
+import time
 KEY_FILEPATH = "../access.txt"
+
+##########################################################
+#
+#  
+#  The machine being used to test this is an Intel Core i5
+#    with 2 cores. At any given time it is therefore useful
+#    to have subprocessing for generating text in images
+#    and one for the rest (downloads can be multithreaded
+#    along with encoding)
+#    
+##########################################################
 
 def download_ims(urls, low, high):
     '''Download images from indices low to high of the list of urls'''
     if (len(urls) < 1): 
         print("No images to download")
     else:
-        return 
+        os.mkdir('./images')
+        os.chdir('./images')
+        
+        for url in urls:
+            wget.download(url, './img')
+
+        os.chdir('..') 
 
 def encode(images_path, fps=30, pix_format='yuv420p', out_format='mp4'):
     '''Encode a video from the numbered images at images_path'''
@@ -19,11 +38,11 @@ def encode(images_path, fps=30, pix_format='yuv420p', out_format='mp4'):
     ]
     print(' '.join(command))
 
-
 def generate_text_images():
+    '''Launch as a subprocess to occur in parallel with downloads'''
     return
 
-def twitter_summary(num):
+def twitter_movie(num):
     tokens = []
     try:
         with open(KEY_FILEPATH, "r") as fp:
@@ -42,17 +61,17 @@ def twitter_summary(num):
     #initialize Tweepy API
     api = tweepy.API(auth)
     public_tweets = api.home_timeline(count=num) #get 100 tweets from timeline
-
-    media_files = set()
+    media_files = []
+    captions = []
     for tweet in public_tweets:
+        captions.append(tweet.text)
         media = tweet.entities.get('media', [])
         if(len(media) > 0):
-            media_files.add(media[0]['media_url'])
-
-
-
-    
+            media_files.append(media[0]['media_url'])
+        else:
+            media_files.append('') #placeholder for just text tweets
 
 if __name__ == "__main__":
-    encode('')
-    #twitter_summary(20)
+    start_time = time.time()
+    twitter_movie(20)
+    print("Twitter movie runtime: %.3f s" % (time.time()-start_time))
