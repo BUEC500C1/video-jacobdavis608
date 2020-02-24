afbb266d5a04e600476a9b080ecfc5bc9af850d4
Jacob Davis <jacobdavis608@gmail.com>
2020-02-24 18:16:03 -0500

    Finished simple pipeline without multithreading. Adding 20 sample tweets as a pickle file to be used in unit tests.

--- twitter_movie.py
+++ twitter_movie.py
@@ -1,8 +1,12 @@
 import tweepy
 import wget
 import os
+import shutil
 import threading
 import time
+import textwrap
+import pickle as pkl
+from PIL import Image, ImageDraw, ImageFont
 KEY_FILEPATH = "../access.txt"
 
 ##########################################################
@@ -16,33 +20,89 @@ KEY_FILEPATH = "../access.txt"
 #    
 ##########################################################
 
-def download_ims(urls, low, high):
-    '''Download images from indices low to high of the list of urls'''
-    if (len(urls) < 1): 
+def download_and_caption(media, background_size=(1440,900), image_size=(960, 720)):
+    '''Download images using the input media'''
+    if (len(media) < 1): 
         print("No images to download")
     else:
-        os.mkdir('./images')
-        os.chdir('./images')
-        
-        for url in urls:
-            wget.download(url, './img')
+        #download the images
+        try:
+            for key in media.keys():
+                img_file = './img_raw%d.jpg' % key
+                wget.download(media[key][2], img_file) #download image
 
-        os.chdir('..') 
+                #paste image on a white background under caption
+                img = Image.open(img_file, 'r')
+                img_w, img_h = img.size
+                background = Image.new('RGB', background_size, (29, 161, 242))
+
+                bg_w, bg_h = background.size
+                offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+                background.paste(img, offset)
+
+                #caption image
+                d = ImageDraw.Draw(background)
+                font = ImageFont.truetype('../fonts/HelveticaNeue.ttf', 36) #Choose twitter font
+                tweet_template = "{0}: \n\n{1}"
+                formatted_text = '\n'.join(textwrap.wrap(media[key][0], width=50)) #wrap tweet text
+                d.text((10,10), tweet_template.format(media[key][1], formatted_text), font=font, fill=(255, 255, 255))
+                
+                background.save('./img%d.jpg' % key)
+                os.remove('./img_raw%d.jpg' % key)
+                
+        except Exception as e:
+            print(e)
+        
+        os.chdir('..')
+
+def init_images_folder():
+    '''Set up images folder for processing'''
+    if (os.path.isdir('./images')): #remove if already present
+            shutil.rmtree('./images')   #remove all contents
+    os.mkdir('./images')
+    os.chdir('./images')
 
 def encode(images_path, fps=30, pix_format='yuv420p', out_format='mp4'):
     '''Encode a video from the numbered images at images_path'''
+    
     command = [
-        'ffmpeg', '-framerate', '1/3', '-i', 'img%d.png', 
+        'ffmpeg', '-framerate', '1/3', '-i', '{0}/img%d.jpg'.format(images_path), 
         '-r', '%d' % fps, '-pix_fmt', '%s' % pix_format, 
         'out.%s' % out_format
     ]
-    print(' '.join(command))
+    com = ' '.join(command)
+    os.system(com)
+    #print("\n\n%s" % com)
 
-def generate_text_images():
+def generate_text_images(text_tweets, im_size=(1440,900)):
     '''Launch as a subprocess to occur in parallel with downloads'''
+    tweet_template = "{0}: \n\n{1}"
+    if (not os.path.isdir('./images')):
+        os.mkdir('./images')
+    for num in text_tweets.keys():
+        img = Image.new('RGB', im_size, color=(29, 161, 242))      #make a background with twitter color
+        font = ImageFont.truetype('./fonts/HelveticaNeue.ttf', 36) #Choose twitter font
+        
+        d = ImageDraw.Draw(img)
+        formatted_text = '\n'.join(textwrap.wrap(text_tweets[num][0], width=50)) #wrap tweet text
+        d.text((10,10), tweet_template.format(text_tweets[num][1], formatted_text), font=font, fill=(255, 255, 255))
+        img.save('./images/img%d.jpg' % num)
+
     return
 
-def twitter_movie(num):
+def save_tweets_pickle(api, num_tweets):
+    '''Save the home timeline tweets as a pickle file'''
+    tweets = api.home_timeline(count=num_tweets)
+    with open('sample_tweets.p', 'wb') as pkl_file:
+        pkl.dump(tweets, pkl_file)
+
+def load_tweets_pickle():
+    '''Load tweets from saved file'''
+    with open('sample_tweets.p', 'rb') as pkl_file:
+        return pkl.load(pkl_file)
+
+def twitter_movie(num=20, get_new_tweets=False):
+    '''Create twitter feed summary for num tweets. Option to get more tweets from twitter'''
     tokens = []
     try:
         with open(KEY_FILEPATH, "r") as fp:
@@ -58,20 +118,39 @@ def twitter_movie(num):
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    #initialize Tweepy API
+    #Use Tweepy API to get the latest "num" tweets
     api = tweepy.API(auth)
-    public_tweets = api.home_timeline(count=num) #get 100 tweets from timeline
-    media_files = []
-    captions = []
-    for tweet in public_tweets:
-        captions.append(tweet.text)
-        media = tweet.entities.get('media', [])
+    
+    #Get tweets either from local or from twitter
+    if (get_new_tweets):
+        public_tweets = api.home_timeline(count=num) #get num tweets from timeline
+    else:
+        public_tweets = load_tweets_pickle()
+    
+    media_tweets = {} #dictionary that maps a tweet number to its media url and text
+    text_tweets = {}  #dictionary that maps a tweet number to just its text
+    for i in range(len(public_tweets)):
+        text = public_tweets[i].text
+        user = public_tweets[i].user.name
+        media = public_tweets[i].entities.get('media', [])
         if(len(media) > 0):
-            media_files.append(media[0]['media_url'])
+            media_tweets[i] = (text, user, media[0]['media_url'])
         else:
-            media_files.append('') #placeholder for just text tweets
+            text_tweets[i] = (text, user)
+
+    #Initialize images folder
+    init_images_folder()
+
+    # Download images and caption them
+    download_and_caption(media_tweets)
+
+    #Generate text images
+    generate_text_images(text_tweets)
+
+    #Generate video
+    encode('./images')
 
 if __name__ == "__main__":
     start_time = time.time()
-    twitter_movie(20)
-    print("Twitter movie runtime: %.3f s" % (time.time()-start_time))
+    twitter_movie()
+    print("\n\nTwitter movie runtime: %.3f s" % (time.time()-start_time))
