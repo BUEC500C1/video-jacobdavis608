5666623f4766876785ad5fc1616466e65ed770f6
Jacob Davis <jacobdavis608@gmail.com>
2020-02-27 00:01:52 -0500

    Removing obsolete test

--- test_movie.py
+++
@@ -1,9 +0,0 @@
-import twitter_movie
-import os
-
-def test_movie():
-    twitter_movie.twitter_movie()
-    if (os.path.isfile('./out.mp4')):
-        assert 1 == 1
-    else:
-        assert 0 == 1
